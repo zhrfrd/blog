595dc8b87f1f14f312cdbedf596f1f3908c6564f
Farid Zouheir <faridzouheir@farids-mbp.home>
2022-01-11 20:39:47 +0000

    Episode 12

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,22 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post {
-    public static function all() {
-        $files = File::files(resource_path("posts/"));
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
 
-        return array_map(fn($file) => $file->getContents(), $files);
+    public function __construct($title, $excerpt, $date, $body, $slug) {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
+    public static function all() {
+        return collect(File::files(resource_path("posts")))
+        ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+        ->map(fn ($document) => new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+        ));
     }
 
     //Find post by its slug
     public static function find($slug) {
-        base_path();
-
-        if (! file_exists($path = resource_path("posts/{$slug}.html")))
-            throw new ModelNotFoundException();
-
-        //Enable caching of post
-        return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+        //Of all the blog posts, find the one with a slug that matches the one that was requested
+        return static::all()->firstWhere('slug', $slug);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
