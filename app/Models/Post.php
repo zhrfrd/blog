d99efa63ac03421795cc4781611b31dacbaab3d6
Farid Zouheir <faridzouheir@farids-mdp.home>
2022-01-12 21:12:22 +0000

    Episode 20

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,55 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post {
-    public $title;
-    public $excerpt;
-    public $date;
-    public $body;
-    public $slug;
-
-    public function __construct($title, $excerpt, $date, $body, $slug) {
-        $this->title = $title;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->body = $body;
-        $this->slug = $slug;
-    }
-
-    public static function all() {
-        return cache()->rememberForever('posts.all', function () {
-            return collect(File::files(resource_path("posts")))
-                ->map(fn ($file) => YamlFrontMatter::parseFile($file))
-                ->map(fn ($document) => new Post(
-                        $document->title,
-                        $document->excerpt,
-                        $document->date,
-                        $document->body(),
-                        $document->slug
-                ))
-                ->sortByDesc('date');   //Sort posts by descending date
-        });
-    }
-
-    //Find post by its slug
-    public static function find($slug) {
-        //Of all the blog posts, find the one with a slug that matches the one that was requested
-        return static::all()->firstWhere('slug', $slug);
-    }
-
-    //Track post and throw an exception if it couldn't find it
-    public static function findOrFail($slug) {
-        //Of all the blog posts, find the one with a slug that matches the one that was requested
-        $post = static::find($slug);
-
-        if (! $post){   //If the slug doesn't match any post throw exception (404 error message)
-            throw new ModelNotFoundException();
-        }
-
-        return $post;
-    }
+class Post extends Model
+{
+    use HasFactory;
 }
--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,9 @@ Route::get('/', function () {
 });
 
 
-Route::get('posts/{post}', function ($slug) {
+Route::get('posts/{post}', function ($id) {
     //Find a post by its slug and pass it to a view called "post"
     return view('post', [
-        'post' => Post::findOrFail($slug)
+        'post' => Post::findOrFail($id)
     ]);
 });
